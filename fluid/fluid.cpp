fb7809b964446ec1ea7532feb025497e7049c0a6
100472037@alumnos.uc3m.es <Simpaticas_22>
2023-10-30 17:03:25 +0100

    binario

--- fluid/fluid.cpp
+++ fluid/fluid.cpp
@@ -6,8 +6,8 @@
 int main(int argc, char *argv[]) {
     ProgArgs args(argc, argv);
 
-    std::ifstream inputfile(args.inputfile);
-    std::ofstream outputfile(args.outputfile);
+    std::ifstream inputfile(args.inputfile, std::ios::binary);
+    std::ofstream outputfile(args.outputfile, std::ios::binary);
 
     // Aquí va el código de la simulación
 
--- sim/progargs.cpp
+++ sim/progargs.cpp
@@ -20,7 +20,7 @@ ProgArgs::ProgArgs(int argc, char *argv[]) {
         exit(-2);
     }
 
-    std::ifstream test_input(argv[2]);
+    std::ifstream test_input(argv[2], std::ios::binary);
     if(!test_input.is_open()) {
         std::cerr << "Error: Cannot open " << argv[2] << " for reading.\n";
         exit(-3);
@@ -28,7 +28,7 @@ ProgArgs::ProgArgs(int argc, char *argv[]) {
     test_input.close();
     inputfile = argv[2];
 
-    std::ofstream test_output(argv[3]);
+    std::ofstream test_output(argv[3], std::ios::binary);
     if(!test_output.is_open()) {
         std::cerr << "Error: Cannot open " << argv[3] << " for writing.\n";
         exit(-4);
