fab0b7da16c3507e65a58d8e0e3621bffd4b8d3e
100472037@alumnos.uc3m.es <Simpaticas_22>
2023-10-30 17:12:51 +0100

    read binario

--- fluid/fluid.cpp
+++ fluid/fluid.cpp
@@ -3,6 +3,35 @@
 #include <cstdlib>
 #include "../sim/progargs.hpp"
 
+template <typename T>
+  requires(std::is_integral_v<T> or std::is_floating_point_v<T>)
+char * as_writable_buffer(T & value) {
+  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+  return reinterpret_cast<char *>(&value);
+}
+
+template <typename T>
+  requires(std::is_integral_v<T> or std::is_floating_point_v<T>)
+char const * as_buffer(T const & value) {
+  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+  return reinterpret_cast<char const *>(&value);
+}
+
+template <typename T>
+  requires(std::is_integral_v<T> or std::is_floating_point_v<T>)
+T read_binary_value(std::istream & is) {
+  T value{};
+  is.read(as_writable_buffer(value), sizeof(value));
+  return value;
+}
+
+template <typename T>
+  requires(std::is_integral_v<T> or std::is_floating_point_v<T>)
+void write_binary_value(T value, std::ostream & os) {
+  os.write(as_buffer(value), sizeof(value));
+}
+
+
 int main(int argc, char *argv[]) {
     ProgArgs args(argc, argv);
 
