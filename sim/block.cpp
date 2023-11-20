6d9424334172a0ec8db84cf3955ea482acb30c7b
100471948 <100471948@alumnos.uc3m.es>
2023-11-20 18:51:36 +0100

    Changes by luis

--- sim/block.cpp
+++ sim/block.cpp
@@ -14,29 +14,3 @@ void Block::removeParticle(int parti){
   //auto pos = std::find(particles.begin(), particles.end(), p);
   particles.erase(std::remove(particles.begin(), particles.end(), parti), particles.end());
 }
-/*void Block::printParticles() {
-  std::cout << "Particles in the block:" << std::endl;
-  for (const auto& particle : particles) {
-    std::cout << "Hay particulas " << std::endl;
-    std::cout << "Position: (" <<  particle.id<< ", " << particle.px << ", " << particle.py << ", " << particle.pz << ")" << std::endl;
-    // Puedes imprimir otras propiedades de la partícula si lo deseas
-    // Ejemplo: velocidad, índices i, j, k, etc.
-  }
-  std::cout << "End of particles" << std::endl;
-}
-void Block::printBlockAndAdyacentes() {
-  // Imprime el bloque actual
-  std::cout << "Bloque (" << i << ", " << j << ", " << k << "):" << std::endl;
-  printParticles();
-
-  // Imprime la lista de bloques adyacentes
-  std::cout << "Bloques adyacentes:" << std::endl;
-  for (const auto& adyacente : bloques_ady) {
-    //std::cout << "Hay bloques adyacentes:" << std::endl;
-    std::cout << adyacente << std::endl;
-  }
-  std::cout << std::endl;
-}*/
-
-
-
--- sim/block.hpp
+++ sim/block.hpp
@@ -2,21 +2,6 @@
 // Created by luis on 30/10/23.
 //
 
-/*#ifndef PRACTICAAC_BLOCK_HPP
-#define PRACTICAAC_BLOCK_HPP
-
-
-
-class block {
-  public:
-    float sx;
-    float sy;
-    float sz;
-
-    block(float bmax_x, float bmax_y, float bmax_z, float bmin_x, float bmin_y, float bmin_z, grid malla);
-};
-
-#endif //PRACTICAAC_BLOCK_HPP*/
 // Block.hpp
 #ifndef BLOCK_HPP
 #define BLOCK_HPP
