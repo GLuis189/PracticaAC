de949f430ae71f124bbc553fd427ee2ec849434c
GLuis189 <100472006@alumnos.uc3m.es>
2023-11-21 19:52:12 +0100

    commit

--- utest/progargs_test.cpp
+++ utest/progargs_test.cpp
@@ -5,35 +5,24 @@
 #include <cstdio>
 #include <fstream>
 
-/*
-
-#include "gtest/gtest.h"
-#include "../sim/progargs.hpp"
-#include <cstdio>
-#include <fstream>
-
 TEST(ProgArgsTest, InvalidNumberOfArguments) {
-    const char* argv_const[] = {"prog", "100", "input.txt"};
-  char** argv = const_cast<char**>(argv_const);
-  EXPECT_EXIT(ProgArgs(3, argv), ::testing::ExitedWithCode(255), "Error: Invalid number of arguments: 2.\n");
+  std::vector<std::string> const argv = {"prog", "100", "input.txt"};
+  EXPECT_EXIT(ProgArgs(argv.size(), argv), ::testing::ExitedWithCode(255), "Error: Invalid number of arguments: 2.\n");
 }
 
 TEST(ProgArgsTest, NonNumericTimeSteps) {
-    const char* argv_const[] = {"prog", "abc", "input.txt", "output.txt"};
-  char** argv = const_cast<char**>(argv_const);
-  EXPECT_EXIT(ProgArgs(4, argv), ::testing::ExitedWithCode(255), "Error: time steps must be numeric.\n");
+  std::vector<std::string> const argv = {"prog", "abc", "input.txt", "output.txt"};
+  EXPECT_EXIT(ProgArgs(argv.size(), argv), ::testing::ExitedWithCode(255), "Error: time steps must be numeric.\n");
 }
 
 TEST(ProgArgsTest, NegativeTimeSteps) {
-    const char* argv_const[] = {"prog", "-100", "input.txt", "output.txt"};
-  char** argv = const_cast<char**>(argv_const);
-  EXPECT_EXIT(ProgArgs(4, argv), ::testing::ExitedWithCode(254), "Error: Invalid number of time steps.\n");
+  std::vector<std::string> const argv = {"prog", "-100", "input.txt", "output.txt"};
+  EXPECT_EXIT(ProgArgs(argv.size(), argv), ::testing::ExitedWithCode(254), "Error: Invalid number of time steps.\n");
 }
 
 TEST(ProgArgsTest, CannotOpenInputFile) {
-    const char* argv_const[] = {"prog", "100", "nonexistent.txt", "output.txt"};
-  char** argv = const_cast<char**>(argv_const);
-  EXPECT_EXIT(ProgArgs(4, argv), ::testing::ExitedWithCode(253), "Error: Cannot open nonexistent.txt for reading.\n");
+  std::vector<std::string> const argv = {"prog", "100", "nonexistent.txt", "output.txt"};
+  EXPECT_EXIT(ProgArgs(argv.size(), argv), ::testing::ExitedWithCode(253), "Error: Cannot open nonexistent.txt for reading.\n");
 }
 
 TEST(ProgArgsTest, CannotOpenOutputFile) {
@@ -50,11 +39,9 @@ TEST(ProgArgsTest, CannotOpenOutputFile) {
     std::cerr << "Error: No se pudo crear input.txt.\n";
     exit(-5);
   }
-  inputFile.close();
 
-    const char* argv_const[] = {"prog", "100", "input.txt", "readonly.txt"};
-  char** argv = const_cast<char**>(argv_const);
-  EXPECT_EXIT(ProgArgs(4, argv), ::testing::ExitedWithCode(252), "Error: Cannot open readonly.txt for writing.\n");
+  std::vector<std::string> const argv = {"prog", "100", "input.txt", "readonly.txt"};
+  EXPECT_EXIT(ProgArgs(argv.size(), argv), ::testing::ExitedWithCode(252), "Error: Cannot open readonly.txt for writing.\n");
 
   // Limpiar
   result = std::system("rm readonly.txt");
@@ -62,6 +49,4 @@ TEST(ProgArgsTest, CannotOpenOutputFile) {
     std::cerr << "Error al eliminar el archivo" << std::endl;
   }
 }
-*/
 
-// NOLINTEND
